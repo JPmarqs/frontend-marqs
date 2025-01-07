ff87182e1f43f9002aff2b8c20d242a13fc1a189
Joao Marques <joaop.marques026@gmail.com>
2025-01-06 22:39:59 -0300

    novos componentes

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
   <Banner/>
   <CallToAction/>
   <Content/>
+  <Form/>
+  <Footer/> 
 </template>
   
 <script setup>
   import Header from './home/header/Header.vue'
   import Banner from './home/banner/Banner.vue'
   import Content from './home/content/Content.vue'
+  import Form from './home/form/Form.vue'
+  import Footer from './home/footer/Footer.vue'
   import CallToAction from './components/callToAction/Button.vue'
 </script>
 
---
+++ src/home/form/Form.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="form-container">
+    <div class="form-content">
+      <h3 class="title-form">
+        Detalhe seu projeto, que em breve entraremos em contato.
+      </h3>
+      <input type="text" placeholder="Nome" />
+      <input type="text" placeholder="E-mail" />
+      <input type="text" placeholder="telefone" />
+      <textarea placeholder="Detalhes do projeto"></textarea>
+      <button class="button-form">Enviar</button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  margin: 20px 0 20px 0;
+}
+.form-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+  width: 70%;
+  height: 100%;
+  margin: 20px 0 20px 0;
+  background-color: #ff8c00;
+  border-radius: 30px;
+}
+.title-form {
+  font-size: 20px;
+  margin-bottom: 30px;
+  text-align: center;
+  color: #0248a7;
+}
+input {
+  margin-bottom: 10px;
+  width: 70%;
+  height: 30px;
+  border-radius: 5px;
+  border: white 2px solid;
+}
+textarea {
+  margin-bottom: 20px;
+  width: 70%;
+  height: 30px;
+  border-radius: 5px;
+  border: white 2px solid;
+}
+button {
+  margin-top: 20px;
+  width: 70%;
+  height: 30px;
+  border-radius: 5px;
+  background-color: #0248a7;
+  color: white;
+  margin-bottom: 20px;
+}
+</style>
